0cf3274a049ac6e17f8eef2f534b770c2cec9ebe
pas2rust <pas2000@proton.me>
2025-10-18 02:14:20 -0300

    bones

--- backend/target/debug/deps/backend-c390c508d38bc916.d
+++
@@ -1,5 +0,0 @@
-/home/pas/Sources/apps/nekoerp/backend/target/debug/deps/backend-c390c508d38bc916.d: src/main.rs
-
-/home/pas/Sources/apps/nekoerp/backend/target/debug/deps/libbackend-c390c508d38bc916.rmeta: src/main.rs
-
-src/main.rs:
--- models/target/debug/deps/models-0a72382a7ce21213.d
+++
@@ -1,5 +0,0 @@
-/home/pas/Sources/apps/nekoerp/models/target/debug/deps/models-0a72382a7ce21213.d: src/main.rs
-
-/home/pas/Sources/apps/nekoerp/models/target/debug/deps/libmodels-0a72382a7ce21213.rmeta: src/main.rs
-
-src/main.rs:
