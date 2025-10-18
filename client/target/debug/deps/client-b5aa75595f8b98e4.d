4810bbb6589ac0bf5c94b6415b5776cb16572ef8
pas2rust <pas2000@proton.me>
2025-10-18 02:19:02 -0300

    setup

---
+++ client/target/debug/deps/client-b5aa75595f8b98e4.d
@@ -0,0 +1,5 @@
+/home/pas/Sources/apps/nekoerp/client/target/debug/deps/client-b5aa75595f8b98e4.d: src/main.rs
+
+/home/pas/Sources/apps/nekoerp/client/target/debug/deps/libclient-b5aa75595f8b98e4.rmeta: src/main.rs
+
+src/main.rs:
